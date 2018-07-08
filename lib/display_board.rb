4c9421d41142be0c708fd951a4648117b6ee5683
Thomas Pope <tpope0928@gmail.com>
2018-07-08 18:10:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-# Define display_board that accepts a board and prints
+board = ["", "", "", "", "", "", "", "", ""]
 def display_board(board)
 -  puts " #{board[0]} | #{board[1]} | #{board[2]} "
 -  puts "-----------"
